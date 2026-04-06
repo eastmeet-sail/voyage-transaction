173e1c76e005cf87159c8701cf6fc5f0c88b74e0
이동우 <ys932184@gmail.com>
2026-04-06 16:53:38 +0900

    chore: PostgreSQL/MySQL 멀티 DB 프로젝트 초기 설정

---
+++ src/main/java/eastmeet/voyage/transaction/VoyageTransactionApplication.java
@@ -0,0 +1,13 @@
+package eastmeet.voyage.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoyageTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoyageTransactionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eastmeet/voyagetransaction/VoyageTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package eastmeet.voyagetransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoyageTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
